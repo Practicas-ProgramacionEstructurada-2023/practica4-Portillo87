5a01015e9e95cdde608171e1547893a19a52c309
Portillo87 <wilfredovillata828@email.com>
2023-09-02 19:05:25 -0600

    Practica4

---
+++ U20230820_Practica4.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace MyApp// Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+         static void Main(string[] args)
+        {
+            Console.WriteLine("\n---------- Usando Sintaxis de estructuras de control selectivas en C# ----------");
+            Console.WriteLine("\nOPCIONES:");
+            Console.WriteLine("1. Determinar si el numero es positivo, negrativo o igual a cero.");
+            Console.WriteLine("2. Mostrar fecha y hora actual.");
+            Console.WriteLine("3. Determinar si puede votar y conducir legalmente.");
+            Console.WriteLine("4 .Determinar si el numero es par o impar.");
+            Console.WriteLine("\nElija una opcion:");
+            int opcion = Convert.ToInt32(Console.ReadLine());
+
+            if (opcion == 1){
+                Console.WriteLine("\nIngrese un numero:");
+                int numero = Convert.ToInt32(Console.ReadLine());
+                if (numero >= 0)
+                {
+                    Console.ForegroundColor= ConsoleColor.Red;
+                    Console.WriteLine("\nSu numero es positivo.\n");
+                }
+
+                else if (numero < 0)
+                {
+                    Console.ForegroundColor= ConsoleColor.Red;
+                    Console.WriteLine("\nSu numero es negativo.\n");
+                }
+                
+                else
+                {
+                    Console.ForegroundColor= ConsoleColor.Red;
+                    Console.WriteLine("\nSu numero es igual a cero.\n");
+                }
+            }
+
+            else if(opcion == 2)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                DateTime FechaHora = DateTime.Now;
+                Console.WriteLine("\nLa fecha y hora actual es: " + FechaHora +"\n");  
+            }
+
+            else
+            {
+                switch (opcion)
+                {
+                    case 3:
+                        Console.WriteLine("\nIngrese su edad:");
+                        int edad = Convert.ToInt32(Console.ReadLine()); 
+                        
+                        if(edad >= 18)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("\nEres mayor de edad, puedes votar y conducir legalmente.\n");
+                        }
+
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("\nNo eres mayor de edad, no puedes votar ni conducir legalmente.\n");
+                        }
+                        break;
+
+                    case 4:
+                        Console.WriteLine("\nIngrese un numero:");
+                        int numero = Convert.ToInt32(Console.ReadLine()); 
+                        
+                        if(numero % 2 == 0)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("\nSu numero ingresado es par\n");
+                        }
+
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("\nSu numero es ingresado es impar.\n");
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("\nOpcion invalida, intente de nuevo.\n");
+                        break;
+                }
+                
+            }
+
+            Console.ReadKey();
+            Console.ResetColor();           
+        }
+    }
+}
